4c6896fe0d00475415dd422c985b4d08d74f31e4
Divyansh Agarwal <74360856+Divyansh-ag14@users.noreply.github.com>
2024-09-23 22:04:24 +0000

    setting up main.py

--- PADS/tic_tac_toe/main.py
+++ PADS/tic_tac_toe/main.py
@@ -1,1 +1,12 @@
 # oop based project
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    choice = "y"
+
+    while choice.lower() == "y":
+        main()
+        choice = input("Want to play again? (y/n): ")
+        
